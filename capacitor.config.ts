dc70824619d0d816ed635883165a1344fb3ec670
Harish Iyer <appsmash89@gmail.com>
2025-06-27 07:44:09 +0000

    i want to bulndle this app using capacitor and create a working apk

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.spinriches.app',
+  appName: 'SpinRiches',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
