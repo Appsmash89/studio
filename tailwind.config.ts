e6740c3745965c1a951995c20a91968f69c08cf9
Harish Iyer <appsmash89@gmail.com>
2025-06-30 19:18:55 +0000

    The flying chip animation speed should be very quick

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -116,7 +116,7 @@ export default {
         'accordion-up': 'accordion-up 0.2s ease-out',
         'flip': 'flip 3s ease-out forwards',
         'bulb-blink': 'bulb-blink 1.5s infinite',
-        'fly-to-target': 'fly-to-target 0.5s ease-out forwards',
+        'fly-to-target': 'fly-to-target 0.3s ease-out forwards',
       },
     },
   },
